b0400f46b8b964fdb2a2be674c99a709b530a67d
aysefat9 <aysefat9@email.com>
2024-12-20 12:44:21 +0300

    Initial commit

---
+++ lib/models/user.dart
@@ -0,0 +1,53 @@
+class User {
+  int id;
+  String username;
+  String email;
+  String firstName;
+  String lastName;
+  String gender;
+  String image;
+  String accessToken;
+  String refreshToken;
+
+  User({
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.gender,
+    required this.image,
+    required this.accessToken,
+    required this.refreshToken,
+  });
+
+  // JSON verisinden User nesnesi oluşturmak için fromJson metodu
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      username: json['username'],
+      email: json['email'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      gender: json['gender'],
+      image: json['image'],
+      accessToken: json['accessToken'],
+      refreshToken: json['refreshToken'],
+    );
+  }
+
+  // User nesnesini JSON formatına çevirmek için toJson metodu
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'username': username,
+      'email': email,
+      'firstName': firstName,
+      'lastName': lastName,
+      'gender': gender,
+      'image': image,
+      'accessToken': accessToken,
+      'refreshToken': refreshToken,
+    };
+  }
+}
